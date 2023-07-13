66f0da2cc69c0d5f3c4534ce350d5d717f0a36f9
ruks2022 <https://github.com/ruks2022/instagram_clone.git>
2023-07-13 10:48:39 +0530

    2nd menu animation

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 var menu = document.getElementById("menu");
+var menu2 = document.getElementById("menu2");
 
 menu.onclick = function myFunction(){
     menu.classList.toggle("openmenu");
+}
+
+menu2.onclick = function myFunction(){
+    menu2.classList.toggle("openmenu2");
 }
